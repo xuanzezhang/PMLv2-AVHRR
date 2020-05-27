09c486b810484d5268fcff5687745dc585195b4a
Andrew X. Zhang <xuanzezhang@hotmail.com>
2020-05-27 09:01:20 +0800

    download GLDAS 3-hourly datasets from NASA
    https://hydro1.gesdisc.eosdis.nasa.gov/data/GLDAS

---
+++ download_GLDAS_NOAH025_3H.2.1-2010.sh
@@ -0,0 +1,1 @@
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --auth-no-challenge=on --keep-session-cookies -c -np -r --content-disposition https://hydro1.gesdisc.eosdis.nasa.gov/data/GLDAS/GLDAS_NOAH025_3H.2.1/2010/
